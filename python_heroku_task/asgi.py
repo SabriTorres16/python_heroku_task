ec58e06285f0742922b6160f9fabc373d5ed614d
sabritorres <sabrinanunez052@gmail.com>
2023-12-12 01:41:47 -0300

    first_commit

--- python_heroku_task/asgi.py
+++ python_heroku_task/asgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_heroku_task.settings')
 
-application = get_asgi_application()
+app = get_asgi_app()
